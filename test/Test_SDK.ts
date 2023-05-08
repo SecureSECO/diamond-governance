fcec63a51a265732e9f7196009a14ecb4975b94b
djeelun <71183944+djeelun@users.noreply.github.com>
2023-05-08 11:15:27 +0200

    fix: get contract through abi
    Main test for verification remains to be fixed

--- test/Test_SDK.ts
+++ test/Test_SDK.ts
@@ -146,7 +146,6 @@ describe("SDK", function () {
 
   // Test to retrieve threshold history
   it("get verification threshold history", async function () {
-    return; // Verification part of SDK needs update
     const { DiamondGovernance } = await loadFixture(deployAragonDAOAndVerifyFixture);
     await getVotingPower(DiamondGovernance);
     const [owner] = await ethers.getSigners();
