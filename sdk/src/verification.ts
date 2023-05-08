1bd0989cb005a60c5a9a09442bf2c8bf3f8be593
djeelun <71183944+djeelun@users.noreply.github.com>
2023-05-08 12:04:59 +0200

    refactor: removed deprecated comment

--- sdk/src/verification.ts
+++ sdk/src/verification.ts
@@ -101,8 +101,7 @@ export class VerificationSugar {
       stamp[2] != null &&
       stamp[2].length > 0 &&
       thresholdHistory != null &&
-      thresholdHistory.length > 0; /* &&
-      currentTimestamp >= lastVerifiedAt.toNumber(); */
+      thresholdHistory.length > 0; 
 
     const expirationDate = lastVerifiedAt
       .add(threshold.mul(24 * 60 * 60))
