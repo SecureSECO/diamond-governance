9c94a87c7de42c7610f685cedfe571159570faca
djeelun <71183944+djeelun@users.noreply.github.com>
2023-06-06 17:06:08 +0200

    feat: optional storage generation

--- scripts/GenerateFacet.ts
+++ scripts/GenerateFacet.ts
@@ -10,16 +10,18 @@ import fs from "fs";
 import path from "path";
 
 const templateFacetFile = "./example-facet/barebones/TemplateFacet.sol.example";
-const templateInterfaceFile = "./example-facet/barebones/ITemplateFacet.sol.example";
-const templateStorageFile = "./example-facet/barebones/LibTemplateStorage.sol.example";
-const iFacetLocation = "./contracts/facets/IFacet.sol"
-const storageDirectory = "./contracts/libraries/storage"
-const outputFile = "./example.facet/NewFacet.sol";
+const templateInterfaceFile =
+  "./example-facet/barebones/ITemplateFacet.sol.example";
+const templateStorageFile =
+  "./example-facet/barebones/LibTemplateStorage.sol.example";
+const iFacetLocation = "./contracts/facets/IFacet.sol";
+const storageDirectory = "./contracts/libraries/storage";
 
 async function main() {
   console.log({
     name: process.env.npm_config_name,
     output: process.env.npm_config_output,
+    includeStorage: process.env.npm_config_includeStorage,
   });
   if (!process.env.npm_config_name) {
     throw new Error("No name provided; use --name=NAME");
@@ -28,7 +30,9 @@ async function main() {
     throw new Error("No output provided; use --output=PATH");
   }
   const name: string = process.env.npm_config_name;
-  const outputDirectory = process.env.npm_config_output;
+  const outputDirectory: string = process.env.npm_config_output;
+  const includeStorage: boolean =
+    process.env.npm_config_includeStorage === "true";
 
   if (!fs.existsSync(outputDirectory)) {
     console.log(`Creating directory: ${outputDirectory}`);
@@ -37,38 +41,55 @@ async function main() {
 
   console.log(`Started generating facet: ${name}Facet.sol`);
 
-
   const outputFacetPath = path.join(outputDirectory, `${name}Facet.sol`);
   const outputInterfacePath = path.join(outputDirectory, `I${name}Facet.sol`);
-  const outputStoragePath = path.join(storageDirectory, `Lib${name}Storage.sol`);
+  const outputStoragePath = path.join(
+    storageDirectory,
+    `Lib${name}Storage.sol`
+  );
   const relativeIFacetPath = path.relative(outputFacetPath, iFacetLocation);
   const relativeStoragePath = path.relative(outputFacetPath, outputStoragePath);
 
   const outputFacet = fs
     .readFileSync(templateFacetFile, "utf-8")
     .replaceAll("Counter", name)
-    .replace("/* INSERT IFACET IMPORT HERE */", `import { IFacet } from "${relativeIFacetPath}";`)
-    .replace("/* INSERT STORAGE IMPORT HERE */", `import { Lib${name}Storage } from "${relativeStoragePath}";`);
-  
+    .replace(
+      "/* INSERT IFACET IMPORT HERE */",
+      `import { IFacet } from "${relativeIFacetPath}";`
+    )
+    .replace(
+      "/* INSERT STORAGE IMPORT HERE */",
+      includeStorage
+        ? `import { Lib${name}Storage } from "${relativeStoragePath}";`
+        : ""
+    );
 
   const outputInterface = fs
     .readFileSync(templateInterfaceFile, "utf-8")
     .replaceAll("Counter", name);
 
-  const nameSplit = name.replace(/([a-z])([A-Z])/g, "$1 $2"); // split on camelCase using regex
-  const nameSplitUpper = nameSplit.toUpperCase().replaceAll(" ", "_"); // convert to uppercase and replace spaces with underscores
-  const nameSplitLower = nameSplit.toLowerCase().replaceAll(" ", "."); // convert to lowercase and replace spaces with dots
-  const outputStorage = fs
-    .readFileSync(templateStorageFile, "utf-8")
-    .replaceAll("COUNTER", nameSplitUpper)
-    .replaceAll("counter", nameSplitLower)
-    .replace("Counter", name); // only the class name needs to be replaced
-  
   fs.writeFileSync(outputFacetPath, outputFacet);
+  console.log(`Finished generating facet: ${outputFacetPath}`);
   fs.writeFileSync(outputInterfacePath, outputInterface);
-  fs.writeFileSync(outputStoragePath, outputStorage);
+  console.log(`Finished generating interface: ${outputInterfacePath}`);
 
-  console.log(`Finished generating facet: ${outputDirectory}/${name}Facet.sol`);
+  if (includeStorage) {
+    const nameSplit = name.replace(/([a-z])([A-Z])/g, "$1 $2"); // split on camelCase using regex
+    const nameSplitUpper = nameSplit.toUpperCase().replaceAll(" ", "_"); // convert to uppercase and replace spaces with underscores
+    const nameSplitLower = nameSplit.toLowerCase().replaceAll(" ", "."); // convert to lowercase and replace spaces with dots
+    const outputStorage = fs
+      .readFileSync(templateStorageFile, "utf-8")
+      .replaceAll("COUNTER", nameSplitUpper)
+      .replaceAll("counter", nameSplitLower)
+      .replace("Counter", name); // only the class name needs to be replaced
+
+    fs.writeFileSync(outputStoragePath, outputStorage);
+    console.log(`Finished generating storage: ${outputStoragePath}`);
+  } else {
+    console.log("Skipping storage generation");
+  }
+
+  console.log();
   console.log(
     "Don't forget to replace placeholder variable in the storage struct!"
   );
