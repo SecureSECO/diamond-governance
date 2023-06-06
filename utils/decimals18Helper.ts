05f7e43ece73f71f98f252b4857615ad3f98fe20
djeelun <71183944+djeelun@users.noreply.github.com>
2023-06-06 12:39:45 +0200

    docs: add comments for helper functions

--- utils/decimals18Helper.ts
+++ utils/decimals18Helper.ts
@@ -1,11 +1,21 @@
 import { BigNumber } from "ethers";
 
+/**
+ * Convert any (decimal) number to 18 decimals
+ * @param amount The number to convert to 18 decimals
+ * @returns The corresponding BigNumber in 18 decimals
+ */
 export const to18Decimal = (amount: number): BigNumber => {
   const { amount: newAmount, exponent } = tenFoldUntilLimit(amount);
 
   return BigNumber.from(newAmount).mul(BigNumber.from(10).pow(18 - exponent));
 };
 
+/**
+ * Multiply a number by 10 until it reaches the maximum safe integer
+ * @param amount The number to multiply
+ * @returns The multiplied number and the exponent
+ */
 export const tenFoldUntilLimit = (
   amount: number
 ): { amount: number; exponent: number } => {
