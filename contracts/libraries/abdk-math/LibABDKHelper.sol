06e7aa326413a2137fb94693acd62aaa2eb58d1c
djeelun <71183944+djeelun@users.noreply.github.com>
2023-06-06 14:38:55 +0200

    chore: removed 64x64 math completely

--- contracts/libraries/abdk-math/LibABDKHelper.sol
+++ contracts/libraries/abdk-math/LibABDKHelper.sol
@@ -6,54 +6,11 @@
 
 pragma solidity ^0.8.0;
 
-import {ABDKMath64x64} from "./ABDKMath64x64.sol";
 import {ABDKMathQuad} from "./ABDKMathQuad.sol";
 
 library LibABDKHelper {
-    // This is a precalculated value of 1e18 in 64.64 fixed point
-    int128 constant DECIMALS_18 = 18446744073709551616000000000000000000;
     // This is a precalculated value of 1e18 in quad float fixed point
     bytes16 constant DECIMALS_18_QUAD = 0x403abc16d674ec800000000000000000;
-
-    /// @notice Convert uint to 64x64 fixed point
-    /// @param input uint to convert
-    /// @return amount 64x64 fixed point number
-    function from18Decimals(uint input) internal pure returns (int128 amount) {
-        // Split number up into float part and integer part
-        uint ipart;
-        uint fpart;
-
-        // Supposedly more gas efficient
-        unchecked {
-            ipart = input / 1e18;
-            fpart = input % 1e18;
-        }
-
-        amount = ABDKMath64x64.fromUInt(ipart);
-        amount = ABDKMath64x64.add(
-            amount,
-            ABDKMath64x64.div(ABDKMath64x64.fromUInt(fpart), DECIMALS_18)
-        );
-    }
-
-    /// @notice Convert 64x64 fixed point to uint (in 18 decimals)
-    /// @param input 64x64 fixed point number
-    /// @return amount uint (in 18 decimals)
-    function to18Decimals(int128 input) internal pure returns (uint amount) {
-        // Split number up into float part and integer part
-        uint ipart;
-        uint fpart;
-
-        ipart = ABDKMath64x64.toUInt(input);
-        fpart = ABDKMath64x64.toUInt(
-            ABDKMath64x64.mul(
-                ABDKMath64x64.sub(input, ABDKMath64x64.fromUInt(ipart)),
-                DECIMALS_18
-            )
-        );
-
-        amount = ipart * 1e18 + fpart;
-    }
 
     function from18DecimalsQuad(
         uint _input
--- test/Test_SearchSECORewardingFacet.ts
+++ test/Test_SearchSECORewardingFacet.ts
@@ -104,7 +104,7 @@ const getERC20MonetaryTokenContract = async (
   return ERC20MonetaryToken;
 };
 
-describe.only("SearchSECORewarding", function () {
+describe("SearchSECORewarding", function () {
   it("should get/set mining reward pool payout ratio correctly", async function () {
     const client = await loadFixture(getClient);
     const ISearchSECORewardingFacet =
