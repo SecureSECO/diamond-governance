d993643f8b41077e9c773b6280577e36acd746a3
djeelun <71183944+djeelun@users.noreply.github.com>
2023-06-08 23:48:16 +0200

    fix: 18 decimal now works properly
    No more 3 decimals bug, no more max safe integer hit, no more queen of england

--- utils/decimals18Helper.ts
+++ utils/decimals18Helper.ts
@@ -2,13 +2,24 @@ import { BigNumber } from "ethers";
 
 /**
  * Convert any (decimal) number to 18 decimals
- * @param amount The number to convert to 18 decimals
+ * @remarks This function can only take numbers in the format of "123.456" (note the dot and no spaces).
+ * @param value The number to convert to 18 decimals (as a string)
  * @returns The corresponding BigNumber in 18 decimals
  */
-export const to18Decimal = (amount: number): BigNumber => {
-  const { amount: newAmount, exponent } = tenFoldUntilLimit(amount);
+export const to18Decimal = (value: string): BigNumber => {
+  const valueAsNumber = Number(value);
+  if (valueAsNumber < 0)
+    throw new Error("Cannot convert negative number to 18 decimals");
 
-  return BigNumber.from(newAmount).mul(BigNumber.from(10).pow(18 - exponent));
+  const valueRounded18 = BigNumber.from(Math.floor(valueAsNumber)).mul(BigNumber.from(10).pow(18));
+
+  if (Math.floor(valueAsNumber) === valueAsNumber) {
+    return valueRounded18;
+  } else {
+    const afterTheComma = value.split(".")[1];
+
+    return valueRounded18.add(BigNumber.from(afterTheComma));
+  }
 };
 
 /**
@@ -30,4 +41,4 @@ export const tenFoldUntilLimit = (
   return { amount: Math.round(amount), exponent: i };
 };
 
-export const DECIMALS_18 = to18Decimal(1);
+export const DECIMALS_18 = to18Decimal("1");
