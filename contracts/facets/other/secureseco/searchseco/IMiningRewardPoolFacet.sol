0750047ffea8bd081bbfd3c6bcabbbde7f99fbf4
djeelun <71183944+djeelun@users.noreply.github.com>
2023-06-12 12:12:36 +0200

    feat: verification rewarding rework
    Now includes reward pool and rewards secoin too

--- contracts/facets/other/secureseco/searchseco/IMiningRewardPoolFacet.sol
+++ contracts/facets/other/secureseco/searchseco/IMiningRewardPoolFacet.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-License-Identifier: MIT
 /**
  * This program has been developed by students from the bachelor Computer Science at Utrecht University within the Software Project course.
  * © Copyright Utrecht University (Department of Information and Computing Sciences)
